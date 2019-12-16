3eeb0fde05eefd64be357c87ea6c6a717995c250
Alan Reid <alanreidt@gmail.com>
2019-12-17 00:09:58 +0300

    Modify Button component Change Button to bem core example

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+
+import { IButtonProps, cnButton } from './index'
+
+export const Button: FC<IButtonProps> = ({
+  children,
+  className,
+  as: Component = 'button',
+  ...props
+}) => (
+  <Component {...props} className={cnButton({}, [className])}>
+    {children}
+  </Component>
+)
